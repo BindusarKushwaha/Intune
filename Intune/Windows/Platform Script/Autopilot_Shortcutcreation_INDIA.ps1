77ea37ef66b78123cbfbd8c35e3a9718be6d7edc
Bindusar Kushwaha <bikush@microsoft.com>
2025-03-29 23:06:16 +0530

    removed comments

--- Intune/Windows/Platform Script/Autopilot_Shortcutcreation_INDIA.ps1
+++ Intune/Windows/Platform Script/Autopilot_Shortcutcreation_INDIA.ps1
@@ -4,9 +4,6 @@ This Sample Code is provided for the purpose of illustration only and is not int
 "This sample script is not supported under any Microsoft standard support program or service. The #sample script is provided AS IS without warranty of any kind. Microsoft further disclaims all implied #warranties including, without limitation, any implied warranties of merchantability or of fitness for a #particular purpose. The entire risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event shall Microsoft, its authors, or anyone else involved in #the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, #without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or #documentation, even if Microsoft has been advised of the possibility of such damages" 
 DISCLAIMER ENDS 
 #>
-# 24-Sep-2021 Adrian 
-#             added code to suppress output in folder creation
-#             added -ErrorAction SilentlyContinue where action can fail
 
 Function Write-Host()
 {
@@ -29,8 +26,15 @@ Function Write-Host()
          [string]$component="ShortCutCreation"
          )
 
-         $logdir="C:\Colt\Logs"        If(!(Test-Path $logdir))        {            $null = New-Item -Path $logdir -ItemType Directory -Force -ErrorAction SilentlyContinue        }
-                $StartTime = Get-Date -Format "dd-MM-yyyy"        [String]$Path = "$Logdir\Autopilot_Custom_$StartTime.log"
+         $logdir="C:\Colt\Logs"
+
+        If(!(Test-Path $logdir))
+        {
+            $null = New-Item -Path $logdir -ItemType Directory -Force -ErrorAction SilentlyContinue
+        }
+        
+        $StartTime = Get-Date -Format "dd-MM-yyyy"
+        [String]$Path = "$Logdir\Autopilot_Custom_$StartTime.log"
         
         $today=Get-Date -Format yyyyMMdd-HH
         $TimeZoneBias = Get-CimInstance -Query "Select Bias from Win32_TimeZone"
